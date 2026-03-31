120e0347e0eaa3e8439638a23e6975ade2320dbc
Nino Markarovi <nino.markarovi@leverx.com>
2026-03-31 21:54:42 +0400

    feat: add testimonials section and styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+    // CTA - Download button
+  const ctaBtn = document.querySelector(".cta__btn");
+  if (ctaBtn) {
+    ctaBtn.setAttribute("href", "/wp-content/uploads/presentation.pdf");
+  }
+});
